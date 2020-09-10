a24fc3f5e765bbcb2d3c8206999afbebce366fa2
YongCoding <aaa9015@naver.com>
2020-09-10 19:25:42 +0900

    JS: main.JS 함수 수정

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 'use strict';
 
 const dateTitle = document.querySelector('.chatting_date');     
-
 let time = document.querySelector('.chatting_time');
-
 let newDate = new Date();           
 
 // 타이틀 날짜 받아오기
@@ -19,7 +17,7 @@ function getDate() {
 getDate();
 
 // 톡 시간(현재시간) 받아오기
-function getTime() {
+function getTime(time) {
     newDate = new Date();
     let hours = newDate.getHours(),
          minutes = newDate.getMinutes();    
@@ -34,15 +32,14 @@ function getTime() {
         `오전 ${hours < 10 ? `${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}`;
     }    
 }
-getTime();
+getTime(time);
 
 // 질문 답변
 const mainContainer = document.querySelector('.chatting_main_container');
-
 const questionContainer = document.querySelector('.chatting_questions_list'),
-questionList = questionContainer.querySelectorAll('li');
-
+    questionList = questionContainer.querySelectorAll('li');
 const audio = new Audio('카톡.mp3');
+let newDiv;
 
 // 카톡 대화시 스크롤 최하로 이동
 function scrollDowun() {
@@ -52,12 +49,11 @@ function scrollDowun() {
 // 채팅 시간 실시간으로 받기
 function chatTimeHandler(div) {
     time = div.querySelector('.chatting_time');    
-    getTime(); 
+    getTime(time); 
 }
-
-// 나의 질문 톡
+// 나의 질문 내용
 function myQuestion(question) {
-    let newDiv = document.createElement("div");
+    newDiv = document.createElement("div");
     newDiv.classList.add('chatting_me');
     mainContainer.append(newDiv);
     newDiv.innerHTML = 
@@ -69,18 +65,15 @@ function myQuestion(question) {
         <p>${question}</p>
     </div>
     </div>`;
-    
-    chatTimeHandler(newDiv); //채팅 시간 실시간 받기
-    scrollDowun(); //스크롤 이동
+    return newDiv;
 }
-// 깐돌이의 답변
-function friendAnswer(answer) {
-    let newDiv = document.createElement("div");
+
+function friendsAnswer(answer) {
+    newDiv = document.createElement("div");
     newDiv.classList.add('chatting_friend');
     mainContainer.append(newDiv);
     newDiv.innerHTML =
-    `<div class="chatting_friend">
-    <div class="chatting_friend_image">
+    `<div class="chatting_friend_image">
         <img src="images/깐돌프사.jpg" />
     </div>
     <div class="chatting_freind_contents">
@@ -98,41 +91,52 @@ function friendAnswer(answer) {
     </div>
     <div class="chatting_time">
        00:00
-    </div>
-</div>`;
-chatTimeHandler(newDiv); //채팅 시간 실간 받기
-audio.play(); //답변과 동시에 카톡알림음
-scrollDowun(); //스크롤 이동
+    </div>`;
+    return newDiv;
+}
+
+// 나의 톡
+function iSay(question) {
+    newDiv = myQuestion(question);
+    chatTimeHandler(newDiv); //채팅 시간 실시간 받기
+    scrollDowun(); //스크롤 이동
+}
+// 깐돌이 톡
+function friendsSay(answer) {
+    newDiv = friendsAnswer(answer);
+    chatTimeHandler(newDiv); //채팅 시간 실간 받기
+    audio.play(); //답변과 동시에 카톡알림음
+    scrollDowun(); //스크롤 이동
 }
 // 질문 리스트 클릭시 
 function questionAnswer() {
 const question = this.className;
 switch (question) {
     case 'question_birthday' :
-        myQuestion('깐돌이의 생일은 언제예요??');
-        setTimeout(friendAnswer,1000,'깐돌이의 생일은<br>2007년 10월 23일날 태어났습니다.^^<br>참고로 누나 몽실이가 있습니다!');       
+        iSay('깐돌이의 생일은 언제예요??');
+        setTimeout(friendsSay,1000,'깐돌이의 생일은<br>2007년 10월 23일날 태어났습니다.^^<br>참고로 누나 몽실이가 있습니다!');       
         break;
     case 'question_birth_place' :
-        myQuestion('깐돌이는 어디서 태어났어용??');
-        setTimeout(friendAnswer,1000,'깐돌이는 경기도 수원시 팔달구 화서동에서 이쁘게 잘 태어났습니다~ 수원견!');
+        iSay('깐돌이는 어디서 태어났어용??');
+        setTimeout(friendsSay,1000,'깐돌이는 경기도 수원시 팔달구 화서동에서 이쁘게 잘 태어났습니다~ 수원견!');
         break;
     case 'question_favorite_food' :
-        myQuestion('깐돌이가 좋아하는 음식은 뭔가요!!??');
-        setTimeout(friendAnswer,1000,'깐돌이는 천하장사 소세지 정말 너무 좋아해요~<br>그래서 까달라고 갖고 올 때도 있고, 쌀튀밥이라는 간식도 정말 좋아한답니다.^^<br>그리고 사람음식도 엄청 좋아했는데 이제는 절대 주지 않고 있어요.');
+        iSay('깐돌이가 좋아하는 음식은 뭔가요!!??');
+        setTimeout(friendsSay,1000,'깐돌이는 천하장사 소세지 정말 너무 좋아해요~<br>그래서 까달라고 갖고 올 때도 있고, 쌀튀밥이라는 간식도 정말 좋아한답니다.^^<br>그리고 사람음식도 엄청 좋아했는데 이제는 절대 주지 않고 있어요.');
         break;
     case 'question_favorite_time' :
-        myQuestion('깐돌이가 좋아하는 시간은 언제죠??');
-        setTimeout(friendAnswer,1000,'깐돌이가 좋아하는 시간은 역시 산책시간이겠죠?^^<br>그리고 형아가 인형가지고 놀아줄 때도 정말 즐겁게 지칠줄 모르고 잘 놀아요!');
+        iSay('깐돌이가 좋아하는 시간은 언제죠??');
+        setTimeout(friendsSay,1000,'깐돌이가 좋아하는 시간은 역시 산책시간이겠죠?^^<br>그리고 형아가 인형가지고 놀아줄 때도 정말 즐겁게 지칠줄 모르고 잘 놀아요!');
         break;
     case 'question_weight' :
-        myQuestion('깐돌이의 몸무게는 몇 키로인가요??');
-        setTimeout(friendAnswer,1000,'깐돌이의 몸무게는 3.8kg 이에요.<br>깐돌이가 젊었을 때는 4.5kg 까지 나갔었답니다:)'); 
+        iSay('깐돌이의 몸무게는 몇 키로인가요??');
+        setTimeout(friendsSay,1000,'깐돌이의 몸무게는 3.8kg 이에요.<br>깐돌이가 젊었을 때는 4.5kg 까지 나갔었답니다:)'); 
         break;
     case 'question_photo' :
-        myQuestion('깐돌이 사진 좀 보여주세요ㅎㅎ');
-        setTimeout(friendAnswer,1000,'<img src="images/깐돌사진1.jpeg" />');
-        setTimeout(friendAnswer,2000,'<img src="images/깐돌사진2.jpeg" />');
-        setTimeout(friendAnswer,3500,'우리 깐돌이 정말 귀엽고 이쁘죠!?');
+        iSay('깐돌이 사진 좀 보여주세요ㅎㅎ');
+        setTimeout(friendsSay,1000,'<img src="images/깐돌사진1.jpeg" />');
+        setTimeout(friendsSay,2000,'<img src="images/깐돌사진2.jpeg" />');
+        setTimeout(friendsSay,3500,'우리 깐돌이 정말 귀엽고 이쁘죠!?');
         break;
 }
 };
---
+++ js/menu.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Menu Show & Hide
+
+const menuBtn = document.querySelector('.chatting_navbar_bars');
+const menuBackground = document.querySelector('.chatting_menu');
+const menuContents = document.querySelector('.chatting_menu_content');
+
+function menuShowHandler() {
+    menuBackground.classList.add('show');
+    menuContents.classList.add('show');
+}
+
+function menuHideHandler() {
+    menuBackground.classList.remove('show');
+    menuContents.classList.remove('show');
+}
+
+menuBtn.addEventListener('click', menuShowHandler); //메뉴 아이콘 클릭시 메뉴 활성화
+menuBackground.addEventListener('click', menuHideHandler); // 검은 배경 클릭시 메뉴 비활성화
